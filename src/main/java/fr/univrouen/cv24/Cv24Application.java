ba3c6402d4a4eb3359096468d063e535006a4b53
Thomas <olivierthomas525@gmail.com>
2024-05-09 20:03:10 +0200

    je desteste clevercloud

--- src/main/java/fr/univrouen/cv24/Cv24Application.java
+++ src/main/java/fr/univrouen/cv24/Cv24Application.java
@@ -2,8 +2,12 @@ package fr.univrouen.cv24;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "fr.univrouen.cv24.repository")
+@EntityScan(basePackages = "fr.univrouen.cv24.model")
 public class Cv24Application {
 
 
